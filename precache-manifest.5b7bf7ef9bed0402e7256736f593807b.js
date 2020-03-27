47cd0163995ab6008e46faca78c39174a158c7b3
Talia Harrison-Marcassa <hm.talia@gmail.com>
2020-03-27 12:06:50 -0400

    Updates

---
+++ precache-manifest.5b7bf7ef9bed0402e7256736f593807b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1efe51f991fcd5172454be7bcfa943fd",
+    "url": "/flexbox-why/index.html"
+  },
+  {
+    "revision": "3a23880470958ecfe653",
+    "url": "/flexbox-why/static/css/main.c0976196.chunk.css"
+  },
+  {
+    "revision": "c6e0631b48a718c9d4f3",
+    "url": "/flexbox-why/static/js/2.0fe37770.chunk.js"
+  },
+  {
+    "revision": "2327f64b2b444dc059ef8cc4743de2ee",
+    "url": "/flexbox-why/static/js/2.0fe37770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a23880470958ecfe653",
+    "url": "/flexbox-why/static/js/main.08118460.chunk.js"
+  },
+  {
+    "revision": "23fd621a2dbc79b54ca8",
+    "url": "/flexbox-why/static/js/runtime-main.9d657fd7.js"
+  },
+  {
+    "revision": "9554bbadbdcb5fc943af6696e4e36633",
+    "url": "/flexbox-why/static/media/book.9554bbad.png"
+  },
+  {
+    "revision": "eb49a30f8cf56eb0700dcfc538b97548",
+    "url": "/flexbox-why/static/media/bookshelf.eb49a30f.png"
+  }
+]);
